bedb419cd56d1cb087366f1eb2ccaead1095052b
pabloFuente <pablofuenteperez@gmail.com>
2017-01-31 19:00:53 +0100

    e2e testing: settings page, tabs, video and chat

--- angular-cli-project/e2e/app.e2e-spec.ts
+++ angular-cli-project/e2e/app.e2e-spec.ts
@@ -3,7 +3,12 @@ import { browser, element, by, protractor } from 'protractor/globals';
 import { AngularCliProjectPage } from './app.po';
 
 describe('angular-cli-project App', function() {
+
   let page: AngularCliProjectPage;
+  let userEmail: string = "teacher@gmail.com";
+  let userPass: string = "pass";
+  let chatMessage: string = "This is a testing message for the chat";
+
   browser.driver.manage().window().maximize();
 
   beforeEach(() => {
@@ -16,7 +21,6 @@ describe('angular-cli-project App', function() {
 
     // Check browser url (root)
     expect(browser.driver.getCurrentUrl()).toMatch('/');
-
     // The menu bar should display the title of the app
     expect(page.getLogoText()).toEqual('FullTeaching');
   });
@@ -28,34 +32,37 @@ describe('angular-cli-project App', function() {
     expect(browser.driver.getCurrentUrl()).toMatch('/');
 
     // Open login modal
-    var loginBtn = browser.driver.findElement(by.id('download-button'));
-    loginBtn.click();
+    browser.driver.findElement(by.id('download-button')).then(function(welcomeBtn){
+      welcomeBtn.click().then(function(){
 
-    // Find form elements
-    var userNameField = browser.driver.findElement(by.id('email'));
-    var userPassField = browser.driver.findElement(by.id('password'));
-    var userLoginBtn = browser.driver.findElement(by.id('log-in-btn'));
+        // Find form elements
+        var userNameField = browser.driver.findElement(by.id('email'));
+        var userPassField = browser.driver.findElement(by.id('password'));
 
-    // Fill input fields
-    userNameField.sendKeys('wrongemail@gmail.com');
-    userPassField.sendKeys('pass');
+        // Fill input fields
+        userNameField.sendKeys('wrongemail@gmail.com');
+        userPassField.sendKeys('pass');
 
-    // Ensure fields contain what has been entered
-    expect(userNameField.getAttribute('value')).toEqual('wrongemail@gmail.com');
-    expect(userPassField.getAttribute('value')).toEqual('pass');
+        // Ensure fields contain what has been entered
+        expect(userNameField.getAttribute('value')).toEqual('wrongemail@gmail.com');
+        expect(userPassField.getAttribute('value')).toEqual('pass');
 
-    // Click to log in - waiting for Angular as it is manually bootstrapped.
-    userLoginBtn.click().then(function() {
-      browser.waitForAngular();
+        browser.driver.findElement(by.id('log-in-btn')).then(function(loginBtn){
+          // Click to log in - waiting for Angular as it is manually bootstrapped.
+          loginBtn.click().then(function() {
+            browser.waitForAngular();
 
-      // Error message appears warning about an invalid field
-      expect(browser.driver.findElement(by.tagName('app-error-message')).getText()).toContain('Invalid field');
-      //Route mustn't change
-      expect(browser.driver.getCurrentUrl()).toMatch('/');
+            // Error message appears warning about an invalid field
+            expect(browser.driver.findElement(by.tagName('app-error-message')).getText()).toContain('Invalid field');
+            //Route mustn't change
+            expect(browser.driver.getCurrentUrl()).toMatch('/');
 
-      // Clear field inputs
-      userNameField.clear();
-      userPassField.clear();
+            // Clear field inputs
+            userNameField.clear();
+            userPassField.clear();
+          });
+        });
+      });
     });
   });
 
@@ -68,22 +75,23 @@ describe('angular-cli-project App', function() {
     // Find form elements (login modal is already opened)
     var userNameField = browser.driver.findElement(by.id('email'));
     var userPassField = browser.driver.findElement(by.id('password'));
-    var userLoginBtn = browser.driver.findElement(by.id('log-in-btn'));
 
     // Fill input fields
-    userNameField.sendKeys('teacher@gmail.com');
-    userPassField.sendKeys('pass');
+    userNameField.sendKeys(userEmail);
+    userPassField.sendKeys(userPass);
 
     // Ensure fields contain what has been entered
-    expect(userNameField.getAttribute('value')).toEqual('teacher@gmail.com');
-    expect(userPassField.getAttribute('value')).toEqual('pass');
+    expect(userNameField.getAttribute('value')).toEqual(userEmail);
+    expect(userPassField.getAttribute('value')).toEqual(userPass);
 
-    // Click to log in - waiting for Angular as it is manually bootstrapped.
-    userLoginBtn.click().then(function() {
-      browser.waitForAngular();
+    browser.driver.findElement(by.id('log-in-btn')).then(function(loginBtn){
+      // Click to log in - waiting for Angular as it is manually bootstrapped.
+      loginBtn.click().then(function() {
+        browser.waitForAngular();
 
-      // Browser url must have changed to the dashboard route
-      expect(browser.driver.getCurrentUrl()).toMatch('/courses');
+        // Browser url must have changed to the dashboard route
+        expect(browser.driver.getCurrentUrl()).toMatch('/courses');
+      });
     });
   });
 
@@ -92,33 +100,37 @@ describe('angular-cli-project App', function() {
 
     // Check browser url (dashboard)
     expect(browser.driver.getCurrentUrl()).toMatch('/courses');
-    page.waitForAnimation();
+
+    page.securePresence('#add-course-icon');
 
     // Open modal for adding a course
-    var addCourseIcon = browser.driver.findElement(by.id('add-course-icon'));
-    addCourseIcon.click();
-    page.waitForAnimation();
+    browser.driver.findElement(by.css('#add-course-icon')).then(function(addCourseIcon){
 
-    // Find form elements
-    var courseNameField = browser.driver.findElement(by.id('inputPostCourseName'));
-    var submitNewCourseBtn = browser.driver.findElement(by.id('submit-post-course-btn'));
+      addCourseIcon.click();
+      page.waitForAnimation();
 
-    // Fill input fields
-    courseNameField.sendKeys('New testing course');
+      // Find form elements
+      var courseNameField = browser.driver.findElement(by.id('inputPostCourseName'));
 
-    // Ensure fields contain what has been entered
-    expect(courseNameField.getAttribute('value')).toEqual('New testing course');
+      // Fill input fields
+      courseNameField.sendKeys('New testing course');
 
-    // Click submit form
-    submitNewCourseBtn.click().then(function() {
-      browser.waitForAngular();
+      // Ensure fields contain what has been entered
+      expect(courseNameField.getAttribute('value')).toEqual('New testing course');
 
-      // The last course should be the one that has been just added
-      var courses = element.all(by.className('course-title'));
-      expect(courses.last().getText()).toContain('New testing course');
+      browser.driver.findElement(by.id('submit-post-course-btn')).then(function(submitNewCourseBtn) {
+        // Click submit form
+        submitNewCourseBtn.click().then(function() {
+          browser.waitForAngular();
 
-      // Clear field inputs
-      courseNameField.clear();
+          // The last course should be the one that has been just added
+          var courses = element.all(by.className('course-title'));
+          expect(courses.last().getText()).toContain('New testing course');
+
+          // Clear field inputs
+          courseNameField.clear();
+        });
+      });
     });
   });
 
@@ -130,27 +142,28 @@ describe('angular-cli-project App', function() {
 
     // Get and click the button to edit the last one of the courses
     var lastEditCoursesIcon = element.all(by.className('course-put-icon')).last();
-    page.waitForAnimation();
     lastEditCoursesIcon.click();
     page.waitForAnimation();
 
     // Get field inputs
     var courseNameField = browser.driver.findElement(by.id('inputPutCourseName'));
-    var submitEditCourseBtn = browser.driver.findElement(by.id('submit-put-course-btn'));
 
     // Fill input field
+    courseNameField.clear();
     courseNameField.sendKeys('[EDITED]');
 
     // Ensure field contains what has been entered
-    expect(courseNameField.getAttribute('value')).toEqual('New testing course[EDITED]');
+    expect(courseNameField.getAttribute('value')).toEqual('[EDITED]');
 
-    // Click submit form
-    submitEditCourseBtn.click().then(function() {
-      browser.waitForAngular();
+    browser.driver.findElement(by.id('submit-put-course-btn')).then(function(submitEditCourseBtn) {
+      // Click submit form
+      submitEditCourseBtn.click().then(function() {
+        browser.waitForAngular();
 
-      //The last course should have changed its name
-      var editedCourse = element.all(by.className('course-title')).last();
-      expect(editedCourse.getText()).toEqual('New testing course[EDITED]');
+        //The last course should have changed its name
+        var editedCourse = element.all(by.className('course-title')).last();
+        expect(editedCourse.getText()).toEqual('[EDITED]');
+      });
     });
   });
 
@@ -171,20 +184,22 @@ describe('angular-cli-project App', function() {
     page.waitForAnimation();
 
     // Get and click the checkbox to allow the course's deletion
-    var deletionAllow = browser.driver.findElement(by.css('label[for=delete-checkbox]'));
-    deletionAllow.click();
+    browser.driver.findElement(by.css('label[for=delete-checkbox]')).then(function(deletionAllow){
+      deletionAllow.click();
 
-    // Get and click the button to delete the course
-    var deleteButton = browser.driver.findElement(by.css('div.delete-div a.btn-flat'));
-    deleteButton.click().then(function() {
-      browser.waitForAngular();
+      // Get and click the button to delete the course
+      browser.driver.findElement(by.css('div.delete-div a.btn-flat')).then(function(deleteButton){
+        deleteButton.click().then(function() {
+          browser.waitForAngular();
 
-      // Get the number of courses present in the dashboard
-      var courses2 = element.all(by.className('course-title'));
-      var j = courses2.count;
+          // Get the number of courses present in the dashboard
+          var courses2 = element.all(by.className('course-title'));
+          var j = courses2.count;
 
-      // Should be exactly one course less than before the deletion
-      expect(Number(j.toString()) === (Number(i.toString()) - 1));
+          // Should be exactly one course less than before the deletion
+          expect(Number(j.toString()) === (Number(i.toString()) - 1));
+        });
+      });
     });
   });
 
@@ -197,37 +212,139 @@ describe('angular-cli-project App', function() {
     // Get and click the last one of the indicators of session inside the calendar view
     var lastSessionIndicatorOfMonth = element.all(by.css('.cal-in-month.cal-has-events .cal-day-badge')).last();
     page.waitForAnimation();
-    lastSessionIndicatorOfMonth.click();
+    lastSessionIndicatorOfMonth.click().then(function(){
 
-    // Get the text of the last of the opened session's shortcuts inside the calendar view
-    element.all(by.css('a.cal-event-title')).last().getText().then(function(text) {
+      // Get the text of the last of the opened session's shortcuts inside the calendar view
+      element.all(by.css('a.cal-event-title')).last().getText().then(function(text) {
 
-      var sessionName = text;
+        var sessionName = text;
 
-      // Get and click the last of the shortcut links to sessions
-      var iconToSession = element.all(by.css('i.material-icons.calendar-event-icon')).last();
-      iconToSession.click().then(function() {
+        // Get and click the last of the shortcut links to sessions
+        var iconToSession = element.all(by.css('i.material-icons.calendar-event-icon')).last();
+        iconToSession.click().then(function() {
+          browser.waitForAngular();
+
+          // Browser url must have changed to the course details page, with the session's tab opened (/courses/[number]/1)
+          expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/1/);
+
+          // Get all the titles of sessions in the course-details page
+          var sessionTitles = element.all(by.css('div.session-title'));
+
+          // Get the session element whose title is equal to the other in the calendar view
+          sessionTitles.filter(function(title) {
+            return title.getText().then(function(text) {
+              return (sessionName.indexOf(text) >= 0)
+            });
+          }).getText().then(function(t) {
+
+            // None of the strings should be empty
+            expect(sessionName.length).not.toEqual(0);
+            expect(t.length).not.toEqual(0);
+
+            // The calendar one should contain the course-details page one
+            expect(sessionName).toMatch(new RegExp(t));
+          });
+        });
+      });
+    });
+  });
+
+  it('should have all the tabs properly working', () => {
+
+    // Check browser url (course-details)
+    expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/[0-4]/);
+
+    // Get all tab bodies
+    var tabBodies = element.all(by.css('.md-tab-body-wrapper .md-tab-body'));
+    // Get all tab buttos
+    var tabButtons = element.all(by.css('.md-tab-label-aux'));
+
+    // For each tab: check the url, check if the active tab-body is the expected and
+    // check if the proper *ngIf has been activated ('div.tab-template-content' should be present)
+    tabButtons.get(0).click().then(function() {
+      expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/0/);
+      expect(tabBodies.get(0).getAttribute('class')).toContain('md-tab-active');
+      expect(element(by.css('.md-tab-active div.tab-template-content')).isPresent()).toBe(true);
+
+      tabButtons.get(1).click().then(function() {
+        expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/1/);
+        expect(tabBodies.get(1).getAttribute('class')).toContain('md-tab-active');
+        expect(element(by.css('.md-tab-active div.tab-template-content')).isPresent()).toBe(true);
+
+        tabButtons.get(2).click().then(function() {
+          expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/2/);
+          expect(tabBodies.get(2).getAttribute('class')).toContain('md-tab-active');
+          expect(element(by.css('.md-tab-active div.tab-template-content')).isPresent()).toBe(true);
+
+          tabButtons.get(3).click().then(function() {
+            expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/3/);
+            expect(tabBodies.get(3).getAttribute('class')).toContain('md-tab-active');
+            expect(element(by.css('.md-tab-active div.tab-template-content')).isPresent()).toBe(true);
+
+            tabButtons.get(4).click().then(function() {
+              expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/4/);
+              expect(tabBodies.get(4).getAttribute('class')).toContain('md-tab-active');
+              expect(element(by.css('.md-tab-active div.tab-template-content')).isPresent()).toBe(true);
+
+              // Back to sessions tab
+              tabButtons.get(1).click();
+            });
+          });
+        });
+      });
+    });
+  });
+
+
+  it('should navigate to video-session page and chat should work', () => {
+    // Check browser url (course-details, tab 1)
+    expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/1/);
+
+    //Get and click on a ready session
+    browser.driver.findElement(by.css('.session-ready')).then(function(sessionReady){
+      sessionReady.click().then(function(){
         browser.waitForAngular();
 
-        // Browser url must have changed to the course details page, with the session's tab opened (/courses/[number]/1)
-        expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/1/);
+        // Check browser url (course-details, tab 1)
+        expect(browser.driver.getCurrentUrl()).toMatch(/\/session\/[0-9]+/);
 
-        // Get all the titles of sessions in the course-details page
-        var sessionTitles = element.all(by.css('div.session-title'));
+        element(by.css('#fixed-icon')).click().then(function(){
+          browser.driver.findElement(by.css('input#message')).then(function(inputChat){
+            // Fill input field
+            inputChat.sendKeys(chatMessage);
+            // Ensure field contain what has been entered
+            expect(inputChat.getAttribute('value')).toEqual(chatMessage);
+            browser.driver.findElement(by.css('input#send-btn')).then(function(sendBtn){
+              sendBtn.click().then(function(){
+                page.waitSeconds(1);
+                element.all(by.css(".user_message")).getText().then(function(userMessages) {
+                  expect(userMessages.indexOf(chatMessage) >= 0);
 
-        // Get the session element whose title is equal to the other in the calendar view
-        sessionTitles.filter(function(title) {
-          return title.getText().then(function(text) {
-            return (sessionName.indexOf(text) >= 0)
+                  element(by.css('#exit-icon')).click().then(function(){
+                    browser.waitForAngular();
+                    expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/1/);
+                  });
+                });
+              });
+            });
           });
-        }).getText().then(function(t) {
+        });
+      });
+    });
+  });
 
-          // None of the strings should be empty
-          expect(sessionName.length).not.toEqual(0);
-          expect(t.length).not.toEqual(0);
 
-          // The calendar one should contain the course-details page one
-          expect(sessionName).toMatch(new RegExp(t));
+  it('should be possible to navigate to settings page', () => {
+    // Navigate to Settings page
+    browser.driver.findElement(by.css('#settings-button')).then(function(settingsButton){
+      settingsButton.click().then(function() {
+        browser.waitForAngular();
+
+        // Browser url must have changed (/settings)
+        expect(browser.driver.getCurrentUrl()).toMatch('/settings');
+        // Settings page should display the user email
+        browser.driver.findElement(by.css('div#stng-user-mail')).then(function(divMail){
+          expect(divMail.getText()).toEqual(userEmail);
         });
       });
     });
@@ -236,24 +353,22 @@ describe('angular-cli-project App', function() {
 
   it('should log out', () => {
 
-    // Check browser url (dashboard)
-    expect(browser.driver.getCurrentUrl()).toMatch(/\/courses\/[0-9]+\/1/);
-
     // Get and click the drop down button in the navbar
-    var arrowBtn = browser.driver.findElement(by.css('#arrow-drop-down'));
-    page.waitForAnimation();
-    arrowBtn.click();
-    page.waitForAnimation();
+    var arrowBtn = browser.driver.findElement(by.css('#arrow-drop-down')).then(function(arrow){
+      arrow.click();
+      page.waitForAnimation();
 
-    // Get the text of the last of the opened session's shortcuts inside the calendar view
-    var logoutButton = browser.driver.findElement(by.css('#logout-button'));
-    logoutButton.click().then(function() {
+      // Get and click the logout button inside the drop-down menu
+      var logoutButton = browser.driver.findElement(by.css('#logout-button')).then(function(logoutBtn) {
+        logoutBtn.click().then(function() {
 
-      // Check browser url (root)
-      expect(browser.driver.getCurrentUrl()).toMatch('/');
+          // Check browser url (root)
+          expect(browser.driver.getCurrentUrl()).toMatch('/');
 
-      //Check if the Welcome button exists
-      expect(browser.driver.findElement(by.css('#download-button')).getText()).toEqual('WELCOME!');
+          //Check if the Welcome button exists
+          expect(browser.driver.findElement(by.css('#download-button')).getText()).toEqual('WELCOME!');
+        });
+      });
     });
   });
 
--- angular-cli-project/e2e/app.po.ts
+++ angular-cli-project/e2e/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, element, by } from 'protractor/globals';
+import { browser, element, by, protractor } from 'protractor/globals';
 
 export class AngularCliProjectPage {
   navigateTo() {
@@ -11,5 +11,15 @@ export class AngularCliProjectPage {
 
   waitForAnimation(){
     browser.sleep(500);
+  }
+
+  waitSeconds(seconds: number){
+    browser.sleep(seconds*1000);
+  }
+
+  securePresence(selector: string){
+    var i = element(by.css(selector));
+    var EC = protractor.ExpectedConditions;
+    browser.wait(EC.presenceOf(i), 3000); // Max 3 seconds
   }
 }
