7e33d0f16847d9cd57b708016fad2a6426b0b9d3
pabloFuente <pablofuenteperez@gmail.com>
2017-02-06 12:37:49 +0100

    http changed to https in production url's

--- angular-cli-project/src/environments/environment.prod.ts
+++ angular-cli-project/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  URL_UPLOAD: 'http://full-teaching-prod.eu-west-1.elasticbeanstalk.com/api-load-files/upload/course/',
-  URL_PIC_UPLOAD: 'http://full-teaching-prod.eu-west-1.elasticbeanstalk.com/api-load-files/upload/picture/',
-  URL_EMAIL_FILE_UPLOAD: 'http://full-teaching-prod.eu-west-1.elasticbeanstalk.com/api-file-reader/upload/course/',
+  URL_UPLOAD: 'https://full-teaching-prod.eu-west-1.elasticbeanstalk.com/api-load-files/upload/course/',
+  URL_PIC_UPLOAD: 'https://full-teaching-prod.eu-west-1.elasticbeanstalk.com/api-load-files/upload/picture/',
+  URL_EMAIL_FILE_UPLOAD: 'https://full-teaching-prod.eu-west-1.elasticbeanstalk.com/api-file-reader/upload/course/',
   PUBLIC_RECAPTCHA_KEY: '6LeYuxIUAAAAAJ38G5U4sYYI0SLZ5JOYRJWrViLe',
   OPENVIDU_URL:'wss://full-teaching-prod.eu-west-1.elasticbeanstalk.com:8443/',
   CHAT_URL: 'wss://full-teaching-prod.eu-west-1.elasticbeanstalk.com/chat'
